99a3563e3c882e902c8e70fd055cda3689ba953e
Joe Legner <joelegner@gmail.com>
2023-11-29 21:23:03 -0500

    Added jsontest

---
+++ jsontest/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+// Address represents the address information in the JSON.
+type Address struct {
+	StreetAddress string `json:"streetAddress"`
+	City          string `json:"city"`
+	State         string `json:"state"`
+	PostalCode    string `json:"postalCode"`
+}
+
+// Grid represents a grid in the JSON.
+type Grid struct {
+	GridName   string `json:"gridName"`
+	GridCoord  string `json:"gridCoord"`
+	GridAngle  string `json:"gridAngle"`
+}
+
+// Project represents the overall structure of the JSON.
+type Project struct {
+	ProjectName string   `json:"projectName"`
+	Address     Address  `json:"address"`
+	Grids       []Grid   `json:"grids"`
+}
+
+func main() {
+	// Read JSON file
+	filePath := "project.json"
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Unmarshal JSON data into struct
+	var project Project
+	err = json.Unmarshal(data, &project)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return
+	}
+
+	// Print the struct
+	fmt.Printf("Project Name: %s\n", project.ProjectName)
+	fmt.Printf("Address: %s, %s, %s %s\n", project.Address.StreetAddress, project.Address.City, project.Address.State, project.Address.PostalCode)
+
+	fmt.Println("Grids:")
+	for _, grid := range project.Grids {
+		fmt.Printf("  Grid Name: %s, Coord: %s, Angle: %s\n", grid.GridName, grid.GridCoord, grid.GridAngle)
+	}
+}
+
