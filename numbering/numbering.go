8005cd01e016bb648775a694bfca18e223b997b5
Joe Legner <joelegner@gmail.com>
2024-02-15 06:18:01 -0500

    Handle and number inline design parameter

--- numbering/numbering.go
+++ numbering/numbering.go
@@ -9,8 +9,8 @@ var index = []int{0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 func readFile() string {
 	return `
-Determine
-    Get input
+Design metal building foundations | Determine program
+    Get input | readFile function
         Modeling Module
             Read input file
             Create model instance
@@ -40,7 +40,7 @@ func calcIndentLevel(line string) int {
 func calculateIndex(line string) string {
 	lineIndentLevel := calcIndentLevel(line)
 	if lineIndentLevel < 0 {
-		return "FR0"
+		return "0"
 	}
 	if lineIndentLevel >= len(index) {
 		lineIndentLevel = len(index) - 1
@@ -50,20 +50,38 @@ func calculateIndex(line string) string {
 	for i := 0; i <= lineIndentLevel; i++ {
 		indexList[i] = fmt.Sprintf("%d", index[i])
 	}
-	return "FR" + strings.Join(indexList, "")
+	return strings.Join(indexList, "")
+}
+
+func splitTextIntoLines(text string) []string {
+	return strings.Split(text, "\n")
+}
+
+func splitLineIntoFRandDP(text string) []string {
+	return strings.Split(text, "|")
 }
 
 func main() {
+	var designParameter string = ""
 	curLevel := 0
 	text := readFile()
-	for _, line := range strings.Split(text, "\n")[1 : len(strings.Split(text, "\n"))-1] {
-		lineLevel := calcIndentLevel(line)
+	for _, line := range splitTextIntoLines(text)[1 : len(strings.Split(text, "\n"))-1] {
+		splitLine := splitLineIntoFRandDP(line)
+		functionalRequirement := splitLine[0]
+		hasDP := (len(splitLine) == 2)
+		if hasDP {
+			designParameter = splitLine[1]
+		}
+		lineLevel := calcIndentLevel(functionalRequirement)
 		if lineLevel < curLevel {
 			// Clear out lower levels when you outdent
 			index = append(index[:lineLevel+1], make([]int, len(index)-lineLevel-1)...)
 		}
 		curLevel = lineLevel
-		lineIndex := calculateIndex(line)
-		fmt.Printf("%s: %s\n", lineIndex, strings.TrimSpace(line))
+		lineIndex := calculateIndex(functionalRequirement)
+		fmt.Printf("FR%s: %s\n", lineIndex, strings.TrimSpace(functionalRequirement))
+		if hasDP {
+			fmt.Printf("DP%s: %s\n", lineIndex, strings.TrimSpace(designParameter))
+		}
 	}
 }
