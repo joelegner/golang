63e1ffa2f24f348533636d681cf0d6b9a284aca3
Joe Legner <joelegner@gmail.com>
2024-08-04 18:18:37 -0400

    Reset rando

--- rando/rando.go
+++ rando/rando.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
-    var numArgs int = len(os.Args)
-    var upperLimit int = 100    // Default
-
-    if numArgs > 2 {
-        fmt.Println("Usage:")
-    }
-    switch numArgs {
-    case: 1
-
-    }
-	fmt.Println(len(os.Args), os.Args)
+	fmt.Println("Joe Legner is cool.")
 }
